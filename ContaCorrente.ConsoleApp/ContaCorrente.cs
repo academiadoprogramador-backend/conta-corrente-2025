c612e9fc68129c711da7cda316aed8196dc4cd1b
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-24 20:15:52 -0300

    Criada classe Movimentacao

--- ContaCorrente.ConsoleApp/ContaCorrente.cs
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -1,16 +1,6 @@
 ﻿
 namespace ContaCorrente.ConsoleApp
 {
-    public class Movimentacao
-    {
-        public decimal valor;
-        public string tipo;
-
-        public string LerMovimentacao()
-        {
-            return $"{tipo} - {valor.ToString("C2")}";
-        }
-    }
 
     public class ContaCorrente
     {
---
+++ ContaCorrente.ConsoleApp/Movimentacao.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ContaCorrente.ConsoleApp
+{
+    public class Movimentacao
+    {
+        public decimal valor;
+        public string tipo;
+
+        public string LerMovimentacao()
+        {
+            return $"{tipo} - {valor.ToString("C2")}";
+        }
+    }
+}
