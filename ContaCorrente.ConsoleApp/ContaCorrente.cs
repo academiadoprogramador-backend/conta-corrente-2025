5937ee04011e56ec165378efebac00c0b829b579
Tiago Santini <tiagosantiniathayde@outlook.com>
2025-04-24 19:50:38 -0300

    Implementada versão inicial com saques, transferências e depósitos

---
+++ ContaCorrente.ConsoleApp/ContaCorrente.cs
@@ -0,0 +1,32 @@
+﻿
+namespace ContaCorrente.ConsoleApp
+{
+    public class ContaCorrente
+    {
+        // atributos
+        public int numero;
+        public decimal saldo;
+        public decimal limite;
+
+        // metodos
+        public void Sacar(decimal quantia)
+        {
+            if (quantia <= saldo + limite)
+            {
+                saldo -= quantia;
+            }
+        }
+
+        public void Depositar(decimal quantia)
+        {
+            saldo += quantia;
+        }
+
+        public void TransferirPara(ContaCorrente destinatario, decimal quantia)
+        {
+            destinatario.Depositar(quantia);
+
+            this.Sacar(quantia);
+        }
+    }
+}
--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace ContaCorrente.ConsoleApp
+﻿
+namespace ContaCorrente.ConsoleApp
 {
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ContaCorrente conta1 = new ContaCorrente();
+            conta1.numero = 12;
+            conta1.saldo = 1000;
+            conta1.limite = 0;
+
+            conta1.Sacar(200);
+
+            conta1.Depositar(300);
+
+            conta1.Depositar(500);
+
+            conta1.Sacar(200);
+
+            ContaCorrente conta2 = new ContaCorrente();
+            conta2.saldo = 300;
+            conta2.numero = 13;
+            conta2.limite = 0;
+
+            conta1.TransferirPara(conta2, 400);
+
+            Console.WriteLine(conta1.saldo);
+
+            Console.ReadLine();
         }
     }
 }
